35d2f907718076488d16e1e7dfa7a5c8a6f92680
Jacob Garneau <jacobg17@hotmail.fr>
2021-12-09 01:34:57 -0500

    Fix attempt 14

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ let express = require("express");
 const portNumber = process.env.PORT || 8081;
 let app = express(); //make an insatnce of express
 
-let httpServer = require("http").createServer(app);
+let httpServer = require("https").createServer(app);
 // create a server (using the Express framework object)
 // declare io which mounts to our httpServer object (runs on top ... )
 let io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://0.0.0.0:8080",
+    origin: "https://0.0.0.0:8080",
     credentials: true,
   },
 });
