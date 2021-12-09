8f3ecbfc7b240fa785f0744c55a52b8a5b90c698
Jacob Garneau <jacobg17@hotmail.fr>
2021-12-09 00:01:12 -0500

    Changed port to 5000

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let tilesetSchema = new Schema({
 let Tile = mongoose.model("Tile", tilesetSchema);
 
 let express = require("express");
-const portNumber = 80;
+const portNumber = 5000;
 let app = express(); //make an insatnce of express
 app.set("port", process.env.PORT || 5000);
 app.set("PORT", process.env.PORT || 5000);
@@ -41,7 +41,7 @@ let httpServer = require("http").createServer(app);
 // declare io which mounts to our httpServer object (runs on top ... )
 let io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:80",
+    origin: "http://localhost:5000",
     credentials: true,
   },
 });
